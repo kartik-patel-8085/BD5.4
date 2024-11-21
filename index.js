959cf208e674777ea94b95e9d280146654dc2cc2
kartik-patel-8085 <trainings@leons-group.com>
2024-11-21 15:31:09 +0530

    comiit

--- index.js
+++ index.js
@@ -1,103 +1,103 @@
-let express = require('express');
-let { track } = require('./models/track_model');
-let { sequelize } = require('./lib/index');
-const { read } = require('fs');
-const { AsyncLocalStorage } = require('async_hooks');
-const { user } = require('./models/user.model');
+let express = require("express");
+let { track } = require("./models/track_model");
+let { sequelize } = require("./lib/index");
+const { read } = require("fs");
+const { AsyncLocalStorage } = require("async_hooks");
+const { user } = require("./models/user.model");
 let app = express();
 app.use(express.json());
 let movieData = [
   {
-    name: 'Raabta',
-    genre: 'Romantic',
+    name: "Raabta",
+    genre: "Romantic",
     release_year: 2012,
-    artist: 'Arijit Singh',
-    album: 'Agent Vinod',
+    artist: "Arijit Singh",
+    album: "Agent Vinod",
     duration: 4,
   },
   {
-    name: 'Naina Da Kya Kasoor',
-    genre: 'Pop',
+    name: "Naina Da Kya Kasoor",
+    genre: "Pop",
     release_year: 2018,
-    artist: 'Amit Trivedi',
-    album: 'Andhadhun',
+    artist: "Amit Trivedi",
+    album: "Andhadhun",
     duration: 3,
   },
   {
-    name: 'Ghoomar',
-    genre: 'Traditional',
+    name: "Ghoomar",
+    genre: "Traditional",
     release_year: 2018,
-    artist: 'Shreya Ghoshal',
-    album: 'Padmaavat',
+    artist: "Shreya Ghoshal",
+    album: "Padmaavat",
     duration: 3,
   },
   {
-    name: 'Bekhayali',
-    genre: 'Rock',
+    name: "Bekhayali",
+    genre: "Rock",
     release_year: 2019,
-    artist: 'Sachet Tandon',
-    album: 'Kabir Singh',
+    artist: "Sachet Tandon",
+    album: "Kabir Singh",
     duration: 6,
   },
   {
-    name: 'Hawa Banke',
-    genre: 'Romantic',
+    name: "Hawa Banke",
+    genre: "Romantic",
     release_year: 2019,
-    artist: 'Darshan Raval',
-    album: 'Hawa Banke (Single)',
+    artist: "Darshan Raval",
+    album: "Hawa Banke (Single)",
     duration: 3,
   },
   {
-    name: 'Ghungroo',
-    genre: 'Dance',
+    name: "Ghungroo",
+    genre: "Dance",
     release_year: 2019,
-    artist: 'Arijit Singh',
-    album: 'War',
+    artist: "Arijit Singh",
+    album: "War",
     duration: 5,
   },
   {
-    name: 'Makhna',
-    genre: 'Hip-Hop',
+    name: "Makhna",
+    genre: "Hip-Hop",
     release_year: 2019,
-    artist: 'Tanishk Bagchi',
-    album: 'Drive',
+    artist: "Tanishk Bagchi",
+    album: "Drive",
     duration: 3,
   },
   {
-    name: 'Tera Ban Jaunga',
-    genre: 'Romantic',
+    name: "Tera Ban Jaunga",
+    genre: "Romantic",
     release_year: 2019,
-    artist: 'Tulsi Kumar',
-    album: 'Kabir Singh',
+    artist: "Tulsi Kumar",
+    album: "Kabir Singh",
     duration: 3,
   },
   {
-    name: 'First Class',
-    genre: 'Dance',
+    name: "First Class",
+    genre: "Dance",
     release_year: 2019,
-    artist: 'Arijit Singh',
-    album: 'Kalank',
+    artist: "Arijit Singh",
+    album: "Kalank",
     duration: 4,
   },
   {
-    name: 'Kalank Title Track',
-    genre: 'Romantic',
+    name: "Kalank Title Track",
+    genre: "Romantic",
     release_year: 2019,
-    artist: 'Arijit Singh',
-    album: 'Kalank',
+    artist: "Arijit Singh",
+    album: "Kalank",
     duration: 5,
   },
 ];
 
-app.get('/seed_db', async (req, res) => {
+app.get("/seed_db", async (req, res) => {
   try {
     await sequelize.sync({ force: true });
     await track.bulkCreate(movieData);
-    res.status(200).json({ message: 'Database Seeding Successful' });
+    res.status(200).json({ message: "Database Seeding Successful" });
   } catch (error) {
     res
       .status(500)
-      .json({ message: 'Error seeding database', error: error.message });
+      .json({ message: "Error seeding database", error: error.message });
   }
 });
 
@@ -106,11 +106,11 @@ async function fetchAllTrack() {
   return { tracks };
 }
 
-app.get('/tracks', async (req, res) => {
+app.get("/tracks", async (req, res) => {
   try {
     let responce = await fetchAllTrack();
     if (responce.tracks.length === 0) {
-      return res.status(404).json({ message: 'No tracks found.' });
+      return res.status(404).json({ message: "No tracks found." });
     }
     return res.status(200).json(responce);
   } catch (error) {
@@ -122,7 +122,7 @@ async function addNewTrack(trackData) {
   let newTrack = await track.create(trackData);
   return { newTrack };
 }
-app.post('/tracks/new', async (req, res) => {
+app.post("/tracks/new", async (req, res) => {
   try {
     let newTrack = req.body.newTrack;
     let response = await addNewTrack(newTrack);
@@ -141,10 +141,10 @@ async function updateTrackById(id, newTrackData) {
 
   trackDetails.set(newTrackData);
   let updateTrack = await trackDetails.save();
-  return { message: 'Track update successful', updateTrack };
+  return { message: "Track update successful", updateTrack };
 }
 
-app.post('/tracks/update/:id', async (req, res) => {
+app.post("/tracks/update/:id", async (req, res) => {
   try {
     let newTrackData = req.body;
     let id = parseInt(req.params.id);
@@ -152,7 +152,7 @@ app.post('/tracks/update/:id', async (req, res) => {
     let response = await updateTrackById(id, newTrackData);
 
     if (!response.message) {
-      return res.status(404).json({ message: 'No track found with this id' });
+      return res.status(404).json({ message: "No track found with this id" });
     }
     return res.status(200).json(response);
   } catch (error) {
@@ -164,16 +164,16 @@ async function deleteTrackById(id) {
   let destruyTrack = await track.destroy({ where: { id } });
 
   if (deleteTrackById === 0) return {};
-  return { message: 'Track record Deleted' };
+  return { message: "Track record Deleted" };
 }
 
-app.post('/tracks/delete', async (req, res) => {
+app.post("/tracks/delete", async (req, res) => {
   try {
     let id = parseInt(req.body.id);
 
     let response = await deleteTrackById(id);
     if (!response.message) {
-      return res.status(404).json({ message: 'Track not found with this id' });
+      return res.status(404).json({ message: "Track not found with this id" });
     }
     return res.status(200).json(response);
   } catch (error) {
@@ -188,7 +188,7 @@ async function addNewUser(newUser) {
 
   return { newData };
 }
-app.post('/user/new', async (req, res) => {
+app.post("/user/new", async (req, res) => {
   try {
     let newUser = req.body.newUser;
     let response = await addNewUser(newUser);
@@ -199,29 +199,258 @@ app.post('/user/new', async (req, res) => {
   }
 });
 
-async function pdateUserById(id, newUserData) {
+async function updateUserById(id, newUserData) {
   let userDetails = await user.findOne({ where: { id } });
   if (!userDetails) {
     return {};
   }
-  userDetails.save(newUserData);
+
+  userDetails.set(newUserData);
   let updatedUser = await userDetails.save();
 
-  return { message: 'User updated successfuly', updatedUser };
+  return { message: "User updated successfuly", updatedUser };
 }
 
-app.post('/users/update/:id', async (req, res) => {
+app.post("/user/update/:id", async (req, res) => {
   try {
     let newUserData = req.body;
     let id = parseInt(req.params.id);
     let response = await updateUserById(id, newUserData);
 
-    return res.status(200).json({ response });
+    return res.status(200).json(response);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
 app.listen(3000, () => {
-  console.log('Server is Running on port 3000');
+  console.log("Server is Running on port 3000");
+});
+let express = require("express");
+let { track } = require("./models/track_model");
+let { sequelize } = require("./lib/index");
+const { read } = require("fs");
+const { AsyncLocalStorage } = require("async_hooks");
+const { user } = require("./models/user.model");
+let app = express();
+app.use(express.json());
+let movieData = [
+  {
+    name: "Raabta",
+    genre: "Romantic",
+    release_year: 2012,
+    artist: "Arijit Singh",
+    album: "Agent Vinod",
+    duration: 4,
+  },
+  {
+    name: "Naina Da Kya Kasoor",
+    genre: "Pop",
+    release_year: 2018,
+    artist: "Amit Trivedi",
+    album: "Andhadhun",
+    duration: 3,
+  },
+  {
+    name: "Ghoomar",
+    genre: "Traditional",
+    release_year: 2018,
+    artist: "Shreya Ghoshal",
+    album: "Padmaavat",
+    duration: 3,
+  },
+  {
+    name: "Bekhayali",
+    genre: "Rock",
+    release_year: 2019,
+    artist: "Sachet Tandon",
+    album: "Kabir Singh",
+    duration: 6,
+  },
+  {
+    name: "Hawa Banke",
+    genre: "Romantic",
+    release_year: 2019,
+    artist: "Darshan Raval",
+    album: "Hawa Banke (Single)",
+    duration: 3,
+  },
+  {
+    name: "Ghungroo",
+    genre: "Dance",
+    release_year: 2019,
+    artist: "Arijit Singh",
+    album: "War",
+    duration: 5,
+  },
+  {
+    name: "Makhna",
+    genre: "Hip-Hop",
+    release_year: 2019,
+    artist: "Tanishk Bagchi",
+    album: "Drive",
+    duration: 3,
+  },
+  {
+    name: "Tera Ban Jaunga",
+    genre: "Romantic",
+    release_year: 2019,
+    artist: "Tulsi Kumar",
+    album: "Kabir Singh",
+    duration: 3,
+  },
+  {
+    name: "First Class",
+    genre: "Dance",
+    release_year: 2019,
+    artist: "Arijit Singh",
+    album: "Kalank",
+    duration: 4,
+  },
+  {
+    name: "Kalank Title Track",
+    genre: "Romantic",
+    release_year: 2019,
+    artist: "Arijit Singh",
+    album: "Kalank",
+    duration: 5,
+  },
+];
+
+app.get("/seed_db", async (req, res) => {
+  try {
+    await sequelize.sync({ force: true });
+    await track.bulkCreate(movieData);
+    res.status(200).json({ message: "Database Seeding Successful" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error seeding database", error: error.message });
+  }
+});
+
+async function fetchAllTrack() {
+  let tracks = await track.findAll();
+  return { tracks };
+}
+
+app.get("/tracks", async (req, res) => {
+  try {
+    let responce = await fetchAllTrack();
+    if (responce.tracks.length === 0) {
+      return res.status(404).json({ message: "No tracks found." });
+    }
+    return res.status(200).json(responce);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+async function addNewTrack(trackData) {
+  let newTrack = await track.create(trackData);
+  return { newTrack };
+}
+app.post("/tracks/new", async (req, res) => {
+  try {
+    let newTrack = req.body.newTrack;
+    let response = await addNewTrack(newTrack);
+
+    return res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+async function updateTrackById(id, newTrackData) {
+  let trackDetails = await track.findOne({ where: { id } });
+
+  if (!trackDetails) {
+    return {};
+  }
+
+  trackDetails.set(newTrackData);
+  let updateTrack = await trackDetails.save();
+  return { message: "Track update successful", updateTrack };
+}
+
+app.post("/tracks/update/:id", async (req, res) => {
+  try {
+    let newTrackData = req.body;
+    let id = parseInt(req.params.id);
+
+    let response = await updateTrackById(id, newTrackData);
+
+    if (!response.message) {
+      return res.status(404).json({ message: "No track found with this id" });
+    }
+    return res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+async function deleteTrackById(id) {
+  let destruyTrack = await track.destroy({ where: { id } });
+
+  if (deleteTrackById === 0) return {};
+  return { message: "Track record Deleted" };
+}
+
+app.post("/tracks/delete", async (req, res) => {
+  try {
+    let id = parseInt(req.body.id);
+
+    let response = await deleteTrackById(id);
+    if (!response.message) {
+      return res.status(404).json({ message: "Track not found with this id" });
+    }
+    return res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//
+
+async function addNewUser(newUser) {
+  let newData = await user.create(newUser);
+
+  return { newData };
+}
+app.post("/user/new", async (req, res) => {
+  try {
+    let newUser = req.body.newUser;
+    let response = await addNewUser(newUser);
+
+    return res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+async function updateUserById(id, newUserData) {
+  let userDetails = await user.findOne({ where: { id } });
+  if (!userDetails) {
+    return {};
+  }
+
+  userDetails.set(newUserData);
+  let updatedUser = await userDetails.save();
+
+  return { message: "User updated successfuly", updatedUser };
+}
+
+app.post("/user/update/:id", async (req, res) => {
+  try {
+    let newUserData = req.body;
+    let id = parseInt(req.params.id);
+    let response = await updateUserById(id, newUserData);
+
+    return res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is Running on port 3000");
 });
